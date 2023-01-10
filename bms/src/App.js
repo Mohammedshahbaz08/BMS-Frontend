c363d03139ca599714b5721851fabd2155d8321e
Mohd-muneer <mohdmuneeruddin456@gmail.com>
2023-01-10 21:44:42 +0530

    react app setup

---
+++ bms/src/App.js
@@ -0,0 +1,11 @@
+import login from "./pages/login";
+
+function App() {
+  return (
+    <div className="App">
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ bms/src/pages/login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div className='container'>
+        <div className="left">masla</div> 
+        <div className="right">logo</div> 
+
+    </div>
+  )
+}
+
+export default login
