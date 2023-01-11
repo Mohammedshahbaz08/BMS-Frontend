cd0541e3a9134485522ee9d815619bb274b59700
Mohammed Shahbaz <shahbaaz72870@hmail.com>
2023-01-11 16:40:55 +0530

    form alignment , basic validation, converted the input into json object

--- bms/src/pages/login.js
+++ bms/src/pages/login.js
@@ -1,77 +1,72 @@
 import React from "react";
-import { Grid, Button,Link, TextField, Checkbox } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { Grid, Button, Link, TextField, Checkbox } from "@mui/material";
 import "../index.css";
-function login() {
-  return <>
-    <div className="carr">
+function Login(props) {
+  let { register, handleSubmit } = useForm();
+  return (
+    <>
+      <div className="carr">
+        <div className=" left-div">
+          <h5 id="bms">&bull; Billing Management System</h5>
+          <form className="main-form" onSubmit={handleSubmit(onSubmit)}>
+            <h2 className="welcome-head">Welcome Back</h2>
+            <h6 className="welcome-head">
+              Welcome back! Please enter your details
+            </h6>
+            <br></br>
+            <TextField
+              required
+              id="outlined-basic"
+              label="UserName"
+              {...register("UserName", { required: true })}
+            />
+            <br></br>
+            <TextField
+              required
+              id="outlined-basic"
+              label="Password"
+              type="password"
+              {...register("Password", { required: true })}
+            />
+            <Link id="forgot-link" href="#" color="inherit" underline="hover">
+              {"Forgot password"}
+            </Link>
+            <Button
+              id="login-btn"
+              type="submit"
+              variant="contained"
+              style={{
+                maxWidth: "30px",
+                maxHeight: "80px",
+                minWidth: "206px",
+                minHeight: "30px",
+              }}
+            >
+              login
+            </Button>
+            <Button id="google-btn" variant="contained">
+              login with Google
+            </Button>
+            <h6>
+              dont have an account?{" "}
+              <Link href="#" color="inherit" underline="hover">
+                {'"Sign up"'}
+              </Link>{" "}
+            </h6>
+          </form>
+        </div>
 
-      <div className=" left-div">
-
-        <h5>&bull; BMS</h5>
-        <h2>Welcome Back</h2>
-        <h6>Welcome back! Please enter your details</h6>
-        <TextField
-          required
-          id="outlined-basic"
-          label="UserName"
-          variant="outlined"
-        />
-        <TextField
-          required
-          id="outlined-basic"
-          label="Password"
-          type="password"
-          variant="outlined"
-        />
-        <Link href="#" color="inherit" underline="hover">
-          {'Forgot password'}
-        </Link>
-        <Button variant="contained" color="success">
-          Login
-        </Button>
-        <Button variant="contained" color="success">
-          login with Google
-        </Button>
-        <h6>dont have an account? <Link href="#" color="inherit" underline="hover">
-          {'"Sign up"'}
-        </Link> </h6>
-
+        <div className="right-div">
+          <h5>logo</h5>
+        </div>
       </div>
-
-
-
-      <div className="right-div">
-        <h5>logo</h5>
-
-      </div>
-
-
-    </div>
-
-  </>;
+    </>
+  );
 }
 
-export default login;
+function onSubmit(values) {
+  console.log(values);
+}
 
-// Regular method
-
-/*
-
-<TextField
-            required
-            id="outlined-basic"
-            label="UserName"
-            variant="outlined"
-          />
-          <TextField
-            required
-            id="outlined-basic"
-            label="Password"
-            type="password"
-            variant="outlined"
-          />
-          <Checkbox />
-          <Button variant="contained" color="success">
-            Login
-          </Button>
-*/
+export default Login;
